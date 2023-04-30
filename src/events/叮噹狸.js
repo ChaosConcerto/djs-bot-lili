0f50c56d6f80b8ac6058f2fec3dd934d21d3aa8e
kevin <chiangchaos@gmail.com>
2023-04-30 13:49:31 +0800

    [叮噹狸模組化OK]

---
+++ src/events/叮噹狸.js
@@ -0,0 +1,11 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+  name: Events.MessageCreate,
+  async execute(message)  {
+    if (message.content === '叮噹狸') {
+    const replay = await message.reply('你的叮噹狸已上線!')
+      replay.react('✅')
+  };
+}
+}
