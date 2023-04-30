4763024f8bb0c501e77aec4b4c3483002bfa7500
kevin <chiangchaos@gmail.com>
2023-04-30 11:57:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
+require('dotenv').config();
+
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+
+client.on('messageCreate', async (message) => {
+    if (message.content === '叮噹狸') {
+        const replay = await message.reply('你的叮噹狸已上線')
+        replay.react('✅')
+    }
+})
+
+client.on('messageReactionAdd', (reaction) => {
+    console.log(reaction)
+})
+
+client.login(process.env.TOKEN)
