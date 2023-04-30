2e8de546435e0bc762a05d0e87ddebc1bf77e80e
kevin <chiangchaos@gmail.com>
2023-04-30 13:48:30 +0800

    [叮噹狸模組化]

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const fs = require('node:fs');
+const path = require('node:path');
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
@@ -14,20 +16,34 @@ const client = new Client({
 require('dotenv').config();
 
 
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+// client.once(Events.ClientReady, c => {
+// 	console.log(`Ready! Logged in as ${c.user.tag}`);
+// });
 
 
-client.on('messageCreate', async (message) => {
-    if (message.content === '叮噹狸') {
-        const replay = await message.reply('你的叮噹狸已上線!')
-        replay.react('✅')
-    }
-})
+// client.on('messageCreate', async (message) => {
+//     if (message.content === '叮噹狸') {
+//         const replay = await message.reply('你的叮噹狸已上線!')
+//         replay.react('✅')
+//     }
+// })
 
 client.on('messageReactionAdd', (reaction) => {
     console.log(reaction)
 })
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 
 client.login(process.env.TOKEN)
