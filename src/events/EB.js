172e304b5a12bccbb0344d3b20cbcb24b3438f84
ChaosConcerto <edelweiss0727@gmail.com>
2023-05-03 09:27:13 +0800

    [EB]

--- src/events/EB.js
+++ src/events/EB.js
@@ -7,6 +7,10 @@ module.exports = {
     const replay = await message.reply('https://media.discordapp.net/attachments/1023555311459651654/1102183675321384980/IMG_1245.webp')
       replay.react('✅')
   }
+  else if (message.content === '色狗2') {
+    const reply = await message.reply('https://media.discordapp.net/attachments/1020572202917167126/1102915259469152256/IMG_1975.png');
+    reply.react('✅');
+  }
   else if (message.content === '臭狗') {
     const reply = await message.reply('https://media.discordapp.net/attachments/1020572202917167126/1102159653317652550/IMG_1940.png');
     reply.react('✅');
