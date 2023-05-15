c9753e08fdfae4d263303e67d2a190c5fc3e17a8
kevin <chiangchaos@gmail.com>
2023-05-16 05:47:56 +0800

    [池清色色]

--- src/events/池清.js
+++ src/events/池清.js
@@ -6,6 +6,10 @@ module.exports = {
   if (message.content === '池清') {
     const replay = await message.reply('https://media.discordapp.net/attachments/1020572202917167126/1102230955835523174/Z.png')
       replay.react('✅')
+  }
+  else if (message.content === '池清色色') {
+    const reply = await message.reply('https://media.discordapp.net/attachments/1020572202917167126/1107784382346973214/lmtqy-.gif');
+    reply.react('✅');
   };
 }
 }
