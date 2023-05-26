e83bb00380581c90395d6fe77aba5b3ab00be6e2
ChaosConcerto <edelweiss0727@gmail.com>
2023-05-26 22:26:40 +0800

    [晚安]

--- src/events/晚安.js
+++ src/events/晚安.js
@@ -4,7 +4,7 @@ module.exports = {
   name: Events.MessageCreate,
   async execute(message)  {
   if (message.content === '晚安') {
-    const replay = await message.reply('https://media.discordapp.net/attachments/1020572202917167126/1103000971740708864/2Q.png')
+    const replay = await message.reply('https://media.discordapp.net/attachments/1020572202917167126/1111661007480311930/2Q.png')
       replay.react('✅')
   };
 }
